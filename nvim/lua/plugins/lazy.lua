2771e81c3e3e969e8cce145c1377921a0fc30b12
Colin <colin.vandervoort.42@gmail.com>
2024-03-24 13:26:25 -0700

    add .zshrc

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -38,7 +38,7 @@ local plugins = {
   },
   {
     "nvim-telescope/telescope.nvim", tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
   },
   {
     "nvim-treesitter/nvim-treesitter",
