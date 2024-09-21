7c7583ab938cb49b49bfbe99d2f7cbd872118d18
colin-vandervoort-sri <colin.vandervoort@sri.com>
2024-09-21 08:02:21 -0700

    add to list of packages probably installable with winget

--- tools/winget-install-devtools.ps1
+++ tools/winget-install-devtools.ps1
@@ -1,17 +1,28 @@
-$WingetPackages = @(
-    'GitExtensionsTeam.GitExtensions'
-    'GitHub.GitLFS',
-    'Hashicorp.Vagrant',
-    'Microsoft.PowerShell',
-    'Microsoft.VisualStudioCode',
-    'Microsoft.VisualStudioCode.CLI',
-    'Microsoft.WindowsTerminal',
-    'Microsoft.PowerToys',
-    'Neovim.Neovim',
-    'Oracle.VirtualBox',
-    'RedHat.Podman-Desktop'
-)
+# $WingetPackages = @(
+#     'Amazon.AWSCLI',
+#     'Docker.DockerDesktop',
+#     'Git.Git',
+#     'GitExtensionsTeam.GitExtensions'
+#     'GitHub.GitHubDesktop',
+#     'GitHub.GitLFS',
+#     'GoLang.Go',
+#     'Hashicorp.Vagrant',
+#     'Microsoft.PowerShell',
+#     'Microsoft.PowerToys',
+#     'Microsoft.SQLServerManagementStudio',
+#     'Microsoft.VisualStudio.2022.BuildTools',
+#     'Microsoft.VisualStudioCode',
+#     'Microsoft.VisualStudioCode.CLI',
+#     'Microsoft.WindowsTerminal',
+#     'Mozilla.Firefox',
+#     'Neovim.Neovim',
+#     'Obsidian.Obsidian',
+#     'Oracle.VirtualBox',
+#     'RedHat.Podman-Desktop',
+#     'Starship.Starship',
+#     'StephanDilly.gitui'
+# )
 
-foreach ($Package in $WingetPackages) {
-    winget install -e --id $Package
-}
+# foreach ($Package in $WingetPackages) {
+#     winget install -e --id $Package
+# }
