e555fa9894f85bb3a2b08db95af84d994f3c1df5
Colin VanDervoort <colin.vandervoort.42@gmail.com>
2025-10-05 15:54:34 -0700

    feat: organize and improve pwsh config file

--- pwsh/profiles/current-user-current-host/Microsoft.PowerShell_profile.ps1
+++ pwsh/profiles/current-user-current-host/Microsoft.PowerShell_profile.ps1
@@ -2,16 +2,24 @@
 # https://learn.microsoft.com/en-us/powershell/scripting/whats-new/migrating-from-windows-powershell-51-to-powershell-7?view=powershell-7.4
 #
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/set-alias?view=powershell-7.4
+#
+# Testing framework to investigate: https://pester.dev/
+
+function Test-ProgramExists {
+    param (
+        [string]$ProgramName
+    )
+    return (Get-Command $ProgramName -ErrorAction SilentlyContinue | Out-Null)
+}
 
 # Get information about current PowerShell
 # $MajorVersion = $PSVersionTable.PSVersion.Major
 
 # Setup Fast Node Manager
 # https://github.com/Schniz/fnm
-fnm env --use-on-cd --shell power-shell | Out-String | Invoke-Expression
-
-# Remove some default aliases
-Remove-Item -Force alias:gc
+if (Test-ProgramExists fnm) {
+    fnm env --use-on-cd --shell power-shell | Out-String | Invoke-Expression
+}
 
 # Common bash aliases
 Set-Alias -Name "ll" Get-ChildItem
@@ -24,17 +32,30 @@ Set-Alias -Name "pbpaste" Get-Clipboard
 
 # Git aliases
 function GitCommit { & git commit $args }
+Remove-Item -Force alias:gc
 Set-Alias -Name "gc" GitCommit
 
 function GitTag { & git tag $args }
-Set-Alias -Name "gc" GitTag
+Set-Alias -Name "gt" GitTag
 
 function GitSwitch { & git switch $args }
 Set-Alias -Name "gs" GitSwitch
+
+function GitBranch { & git branch $args }
+Set-Alias -Name "gb" GitBranch
 
 # Directory shortcuts
 function Set-Location-To-Home-Dir { Set-Location -Path "~" }
 Set-Alias -Name "homedir" Set-Location-To-Home-Dir
 
+# PowerShell ReadLine options
+$PSReadLineOptions = @{
+    EditMode = "Emacs"
+    HistoryNoDuplicates = $true
+}
+Set-PSReadLineOption @PSReadLineOptions
+
 # Init Starship
-Invoke-Expression (&starship init powershell)
+if (Test-ProgramExists starship) {
+    Invoke-Expression (&starship init powershell)
+}
